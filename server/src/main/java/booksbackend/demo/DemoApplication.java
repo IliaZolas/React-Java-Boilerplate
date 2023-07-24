09cd534d2e1145dc65507ac0052248f05946951b
Ilia Zolas <ilia@thedmcsa.co.za>
2023-07-24 18:00:26 +0200

    added error handling in dependacies and templates, app now starts normally

--- server/src/main/java/booksbackend/demo/DemoApplication.java
+++ server/src/main/java/booksbackend/demo/DemoApplication.java
@@ -2,12 +2,12 @@ package booksbackend.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "booksbackend.demo")
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
